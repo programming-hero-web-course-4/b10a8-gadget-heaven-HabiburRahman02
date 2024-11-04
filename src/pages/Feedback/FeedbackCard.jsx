b82071563269cc1fed22a50bf3a990cf5f2e59ae
Habibur Rahman <habibur88454@gmail.com>
2024-11-04 23:47:19 +0600

    user feedback page added

--- src/pages/Feedback/FeedbackCard.jsx
+++ src/pages/Feedback/FeedbackCard.jsx
@@ -3,17 +3,23 @@ const FeedbackCard = ({ feed }) => {
     const { name, profile_image, rating, comment, date } = feed;
     return (
         <div>
-            <div className="card bg-base-100 shadow-xl">
-                <figure>
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                        alt="Shoes" />
+            <div className="card h-full bg-base-100 shadow-xl rounded-xl">
+                <figure className="mt-4 ">
+                    <div className="relative">
+                        <img
+                            className="h-32 w-32 rounded-full object-cover"
+                            src={profile_image}
+                            alt="Shoes" />
+                        <div className="">
+                            <p className="absolute flex items-center justify-center top-0 right-0 bg-[#9538E2] text-white rounded-full h-8 w-8">{rating}</p>
+                        </div>
+                    </div>
                 </figure>
                 <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+                    <h2 className="card-title">{name}</h2>
+                    <p>{comment}</p>
+                    <div className="card-actions font-medium justify-between">
+                        <p >Date: {date}</p>
                     </div>
                 </div>
             </div>
