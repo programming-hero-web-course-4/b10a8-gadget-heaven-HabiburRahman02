6c71086f6817920d75e59b073459cc5181ded8cb
Habibur Rahman <habibur88454@gmail.com>
2024-11-04 22:31:51 +0600

    total cost get

--- src/pages/Dashboard/Carts/Carts.jsx
+++ src/pages/Dashboard/Carts/Carts.jsx
@@ -5,12 +5,23 @@ import Cart from "./Cart";
 const Carts = () => {
     const { carts } = useContext(ProductContext);
     console.log(carts);
+    const totalCost = carts.reduce((total, item) => total + item.price, 0);
+    console.log(('total', totalCost));
     return (
-        <div className="grid grid-cols-1 gap-6">
-            cart
-            {
-                carts.map(cart => <Cart key={cart.id} cart={cart}></Cart>)
-            }
+        <div className="max-w-[1400px] mx-auto mt-12">
+            <div className="flex justify-between">
+                <h2 className='text-2xl font-semibold'>Cart</h2>
+                <div className="flex gap-6 items-center">
+                    <h2 className='text-2xl font-semibold'>Total cost: ${totalCost.toFixed(2)}</h2>
+                    <button className='btn'>Sort by Price</button>
+                    <button className='btn'>Purchase</button>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 gap-6 mt-6">
+                {
+                    carts.map(cart => <Cart key={cart.id} cart={cart}></Cart>)
+                }
+            </div>
         </div>
     );
 };
--- src/pages/Dashboard/DashboardBanner/Lists/Lists.jsx
+++ src/pages/Dashboard/DashboardBanner/Lists/Lists.jsx
@@ -6,11 +6,13 @@ const Lists = () => {
     const { lists } = useContext(ProductContext);
     console.log(lists);
     return (
-        <div className="grid grid-cols-1 gap-6">
-            cart
-            {
-                lists.map(cart => <Cart key={cart.id} cart={cart}></Cart>)
-            }
+        <div className="max-w-[1400px] mx-auto mt-12">
+            <h2 className='text-2xl font-semibold'>Wishlist</h2>
+            <div className='grid grid-cols-1 gap-6 mt-6'>
+                {
+                    lists.map(cart => <Cart key={cart.id} cart={cart}></Cart>)
+                }
+            </div>
         </div>
     );
 };
