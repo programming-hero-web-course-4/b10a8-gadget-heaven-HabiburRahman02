e3ec9bd7b766ab1cfb9c0ba1382e788008d54378
Habibur Rahman <habibur88454@gmail.com>
2024-11-05 13:51:38 +0600

    added add to cart btn

--- src/pages/Dashboard/DashboardBanner/Lists/List.jsx
+++ src/pages/Dashboard/DashboardBanner/Lists/List.jsx
@@ -1,6 +1,8 @@
 import { RxCrossCircled } from "react-icons/rx";
-const Cart = ({ cart }) => {
-    const { image, product_title, description, price } = cart
+import { Link } from "react-router-dom";
+
+const List = ({ list }) => {
+    const { image, product_title, description, price } = list
     return (
         <div className="border mx-auto md:w-2/3">
             <div className="md:flex justify-between gap-6 bg-white p-6 shadow-xl w-full rounded-xl">
@@ -12,6 +14,9 @@ const Cart = ({ cart }) => {
                         <h2 className="text-2xl font-semibold">{product_title}</h2>
                         <p className="text-gray-500">Description: {description}</p>
                         <p className="text-lg font-semibold">Price: ${price}</p>
+                        <Link to='/'>
+                            <button className="bg-[#9538E2] border-2 transition-all text-white font-semibold rounded-full px-6 py-2 flex items-center gap-2 mt-4">Add To Cart</button>
+                        </Link>
                     </div>
                 </div>
                 <div>
@@ -24,4 +29,4 @@ const Cart = ({ cart }) => {
     );
 };
 
-export default Cart;
+export default List;
--- src/pages/Dashboard/DashboardBanner/Lists/Lists.jsx
+++ src/pages/Dashboard/DashboardBanner/Lists/Lists.jsx
@@ -1,16 +1,16 @@
 import React, { useContext } from 'react';
 import { ProductContext } from '../../../../provider/ProductProvider';
-import Cart from '../../Carts/Cart';
+import List from './List';
 
 const Lists = () => {
     const { lists } = useContext(ProductContext);
-    console.log(lists);
+    // console.log(lists);
     return (
         <div className="max-w-[1400px] mx-auto mt-12">
             <h2 className='text-2xl font-semibold'>Wishlist</h2>
             <div className='grid grid-cols-1 gap-6 mt-6'>
                 {
-                    lists.map(cart => <Cart key={cart.id} cart={cart}></Cart>)
+                    lists.map(list => <List key={list.id} list={list} ></List>)
                 }
             </div>
         </div>
